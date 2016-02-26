61dadd35531b42807bc9c62bf49b997fc18b3a96
himangshu <himangshu@gozoomo.com>
2016-02-26 15:24:31 +0530

    Disabling removeCache
    Signed-off-by: himangshu <himangshu@gozoomo.com>

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var server = prerender({
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
-// server.use(prerender.logger());
+// server.use(prerender.blacklist());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
